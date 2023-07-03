cbf9599d29f9881721e58b3045e25963bfbc1bd7
zhangkunsheng <zhangkunsheng@zaxh.cn>
2023-07-03 18:01:11 +0800

    feat：添加插件dayjs

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import { View, Text } from '@tarojs/components'
+
 import './index.scss'
 
 export default function Index() {
   
-  const [ pagesData ,setPagesData ] = useState<{show:boolean,data:string}>({
+  const [ pagesData ,setPagesData ] = useState<{show:boolean,data:string,time:string}>({
     show: false,
-    data: 'Hello world!333333'
+    data: 'Hello world!333333',
+    time: '',
   })
   const {show, data} = pagesData
 
-  const open = () => {
-    setPagesData({...pagesData,show:!pagesData.show})
+  const open = (val:boolean) => {
+    setPagesData({...pagesData,show:val})
+  }
+
+  const onConfirm = (event) =>{
+    console.log(event);
   }
 
   useEffect(()=>{
@@ -20,7 +26,11 @@ export default function Index() {
 
   return (
     <View className='index'>
-      <van-button type="default" onClick = { open }>按钮</van-button>
+      <van-button type="primary" onClick = { open(true) }>按钮</van-button>
+      <van-calendar
+        show={show}
+        onclose={open(false)}
+        onconfirm={onConfirm} />
       <Text>{ data }</Text>
       { show ? <Text> 1111 </Text> : <Text> 222222 </Text> }
     </View>
